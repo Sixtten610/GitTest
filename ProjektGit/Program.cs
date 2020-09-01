fe388651a4f219f12f335baaedad3d325af7b852
6_10 <60339623+Sixtten610@users.noreply.github.com>
2020-09-01 13:05:52 +0200

    FörstaCommit

---
+++ ProjektGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjektGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
